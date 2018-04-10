4e98a6705bb5f7300a3ac151893d3a694bec78bd
ProfBurch <38228559+ProfBurch@users.noreply.github.com>
2018-04-10 14:21:35 -0700

    Update training.py

---
+++ training.py
@@ -0,0 +1,15 @@
+import random 
+
+x = []
+
+y = []
+
+numPoints = 100
+
+def makeRandomData();
+  
+  for i in range (nunPoints)
+    x.append(random.randint(-50,50)
+    y.append(random.randint(-50,50)
+    
+print(x)
