668dd0bc28852c75795234afc79d3d8988bbac5f
isaac-davis-15 <isaacdaviswa2000@gmail.com>
2018-04-10 19:42:01 -0700

    Just a push to get the updated code in the cloud for a pull to a new device

--- network.py
+++ network.py
@@ -34,3 +34,5 @@ def initNetwork():
 
 def createNetwork():
 	initNetwork()
+
+def think()
--- training.py
+++ training.py
@@ -1,15 +1,18 @@
 import random 
 
 x = []
-
 y = []
 
 numPoints = 100
 
-def makeRandomData();
-  
-  for i in range (nunPoints)
-    x.append(random.randint(-50,50)
-    y.append(random.randint(-50,50)
+def makeRandomData():
+	global x
+	global y
+
+	for i in range(numPoints):
+		x.append(random.randint(-50,50))
+		y.append(random.randint(-50,50))
     
+makeRandomData()
 print(x)
+print(y)
