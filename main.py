530bad14892486f3bdd5d8fb5273f0bd77ad64ab
isaac-davis-15 <isaacdaviswa2000@gmail.com>
2018-04-13 14:36:09 -0700

    Almost Done!!!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import training
 import learning
 import time
 import os
+import userinput
 
 #Set up the Neural Network and Generate Training Data
 network.createNetwork()
@@ -20,4 +21,4 @@ while 1==1:
 	print(score/iderations)
 	learning.trainNetwork()
 	#time.sleep(.1)
-	displayData()
+	userinput.displayData()
