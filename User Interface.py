7a62331ae13630c1a0238e0d7b6d156c4f3eee0e
ProfBurch <38228559+ProfBurch@users.noreply.github.com>
2018-04-17 12:59:32 -0700

    Update User Interface.py

--- User Interface.py
+++ User Interface.py
@@ -14,4 +14,5 @@ def displayData
 	print("This is the curent set of the weights the program is running through." + str(network.weights))
 	print("This is the current score of our point classifyer. In just a few minutes you will have your answer:" learning.scoreNetwork)
 	print("The scores and sets will update every .5 seconds")
-    time.sleep(.5)
+        time.sleep(.5)
+	os.system("cls")
